8845d1c9ba48bee1ef86c756cb5ab8bd0a8a5f5b
IT-AlexandrPN <IT-AlexandrPN@yandex.ru>
2020-10-15 16:36:25 +0900

    1

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ print(num)
 W = 460
 H = 376
 SILVER = (192, 192, 192)
-BLACK = (0, 0, 0,)
+BLACK = (0, 0, 0)
+numeral = ''
+move = 1
+block = 0
+start = 1
+OUTSIDE_BG = (0, -100)
 
 pygame.init()
 pygame.display.set_caption('Угадай число')
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((W, H))
+
+font = pygame.font.SysFont('Arial', 28, True, False)
+font_box = pygame.Surface((W - 20, font.get_height()))
+font_rect = font_box.get_rect(center=(W // 2, H - font.get_height()))
+font2 = pygame.font.SysFont('Arial', 14, False, True)
 
 path = os.path.dirname(os.path.abspath(__file__))
 bg = pygame.image.load(os.path.join(path, 'Image/room.png'))
@@ -21,7 +31,7 @@ bg_rect = bg.get_rect(topleft=(0, 0))
 cat = pygame.image.load(os.path.join(path, 'Image/cat.png'))
 cat_rect = cat.get_rect(center=(70, 220))
 dog = pygame.image.load(os.path.join(path, 'Image/dog.png'))
-dog_rect = dog.get_rect(center=(410, 220))
+dog_rect = dog.get_rect(center=(400, 220))
 owl = pygame.image.load(os.path.join(path, 'Image/owl.png'))
 owl_rect = owl.get_rect(center=(210, 120))
 dialog = pygame.image.load(os.path.join(path, 'Image/dialog.png'))
@@ -29,14 +39,44 @@ dialog_rect = dialog.get_rect()
 dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
 dialog_dog_pos = (dog_rect.x, - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
 
+
+def dialogs(text, pos, owl_text):
+    screen.blit(dialog, pos)
+    screen.blit(fond2.render(text, True, BLACK), (pos[0] + 5 pos[1] + 5))
+    dialog_owl_pos = (owl_rect.x, owl_rect.y - dialog_rect.h)
+    screen.blit(dialog, dialog_owl_pos)
+    screen.blit(fond2.render(owl_text, True, BLACK), (dialog_owl_pos[0] + 5 dialog_owl_pos[1] + 5))
+    pygame.display.update()
+    pygame.time.wait(2000)
+
+
 run = True
 while run:
     for e in pygame.event.get():
-        if e .type == pygame.QUIT or e.type == pygame.KEYUP and e.key == pygame.K_ESCAPE:
+        if e .type == pygame.QUIT:
             run = False
+        # выход из пррограммы при нажатии Esc
+        elif e.type == pygame.KEYUP:
+            if e.key == pygame.K_ESCAPE:
+                run = False
+            # печатает только цифры
+            elif e.unicode.isdecimal() and block == 0:
+                numeral += e.unicode
 
-    screen.blit(bg, bg_rect)
-    screen.blit(cat, cat_rect)
-    screen.blit(dog, dog_rect)
-    screen.blit(owl, dog_rect)
-    pygame.display.update()
+    if block == 0:
+        screen.blit(bg, bg_rect)
+        screen.blit(cat, cat_rect)
+        screen.blit(dog, dog_rect)
+        screen.blit(owl, owl_rect)
+        screen.blit(font_box, font_rect)
+        font_box.fill(SILVER)
+        font_box.blit(
+            font.render(numeral, True, BLACK), (10, 0)
+        )
+        pygame.display.update()
+
+    if start == 1:
+        dialogs(''OUTSIDE_BG, 'Я загадала число')
+        dialogs(''OUTSIDE_BG, 'от 1 до 100')
+        dialogs('Кот твой ход', dialog_dog_pos, 'Отгадай его')
+        start = 0 
